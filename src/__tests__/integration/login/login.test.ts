3fae3d3dc8a4d54cf17903edfa9a1b289a053635
juliomello93 <juliomello@live.com>
2023-01-11 09:04:26 -0300

    feat:add test login and create user

---
+++ src/__tests__/integration/login/login.test.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { DataSource } from "typeorm";
+import { app } from "../../../app";
+import AppDataSource from "../../../data-source";
+import { mockedLoginRequest } from "../../mocks/integration/login.mock";
+
+describe("/login", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => {
+        connection = res;
+      })
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+
+    await request(app).post("/users").send(mockedLoginRequest);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  test("Should be possible to login, method POST", async () => {
+    const response = await request(app).post("/login").send(mockedLoginRequest);
+    expect(response.body).toHaveProperty("token");
+    expect(response.statusCode).toBe(200);
+  });
+});
