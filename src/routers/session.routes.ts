e5c472ea20e2ad5e47d15bfec97d686877f4e3cb
juliomello93 <juliomello@live.com>
2023-01-11 16:55:15 -0300

    feat:fix login tests and projects interfaces

--- src/routers/session.routes.ts
+++ src/routers/session.routes.ts
@@ -1,9 +1,6 @@
 import { Router } from "express";
 import { createSessionController } from "../controllers/session.controllers";
-import { ensureDataIsValidMiddleware } from "../middlewares/ensureDataIsValid.middleware";
-import { userLoginSerializer } from "../serializers/users.serializers";
 
-export const sessionRoutes = Router()
+export const sessionRoutes = Router();
 
-sessionRoutes.post("",ensureDataIsValidMiddleware(userLoginSerializer) , createSessionController)
-
+sessionRoutes.post("", createSessionController);
