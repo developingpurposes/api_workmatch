f20016c200cfcbf9f57320920ae202abb5c7d168
anaadx <anapaula.assisduarte.99@gmail.com>
2023-01-11 12:35:37 -0300

    create: more interfaces (IQueue/IProjectCompleteResponse)

--- src/interfaces/projects/index.ts
+++ src/interfaces/projects/index.ts
@@ -1,3 +1,5 @@
+import { ITechnology } from "../technologies";
+
 export interface IProject {
     id: string;
     name: string;
@@ -21,4 +23,24 @@ export interface IProject {
     updatedAt: Date;
     deletedAt: Date;
     userId: string;
+  }
+
+  export interface IQueue {
+    isConfirmed: boolean;
+    userId: string;
+    projectId: string;
+  }
+
+  export interface IProjectCompleteResponse{
+    name: string;
+    imgUrl: string;
+    description: string;
+    maxTeamSize: number;
+    isActive: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date;
+    userId: string;
+    queueList: IQueue[];
+    techsList: ITechnology[];
   }
