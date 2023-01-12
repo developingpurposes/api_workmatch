a3aa284366bd14b3bebd1d9103c7d5130ef4781a
anaadx <anapaula.assisduarte.99@gmail.com>
2023-01-12 09:08:44 -0300

    feat: tests verifications

--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -5,10 +5,13 @@ import {
   listUsersController,
   patchUserController,
 } from "../controllers/users.controller";
+import { ensureIsAdminMiddleware } from "../middlewares/ensureIsAdmin.middleware";
+import { ensureUpdateDataIsRightMiddleware } from "../middlewares/ensureUpdateDataIsRight.middleware";
+import { ensureAuthMiddleware } from "../middlewares/esureAuth.middleware";
 
 export const userRoutes = Router();
 
 userRoutes.post("", createUserController);
-userRoutes.get("", listUsersController);
-userRoutes.patch("/:id", patchUserController);
-userRoutes.delete("/:id", deleteUserController);
+userRoutes.get("", ensureAuthMiddleware, ensureIsAdminMiddleware, listUsersController);
+userRoutes.patch("/:id", ensureAuthMiddleware, ensureUpdateDataIsRightMiddleware, patchUserController);
+userRoutes.delete("/:id", ensureAuthMiddleware, ensureIsAdminMiddleware, deleteUserController);
