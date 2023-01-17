570cc6a564241ca9cb96d61501fcee4e071383f8
Thiago Rodrigues <thiago.rds@outlook.com.br>
2023-01-17 02:03:10 -0400

    feat: crated route, controller, service, send email, middleware  of forgot password

--- src/routers/users/user.routes.ts
+++ src/routers/users/user.routes.ts
@@ -7,15 +7,17 @@ import { listUsersController } from "../../controllers/users/listUsers.controlle
 import { getUserController } from "../../controllers/users/getUser.controller";
 import { patchUserController } from "../../controllers/users/patchUser.controller";
 import { deleteUserController } from "../../controllers/users/deleteUser.controller";
-import { ensureDataIsValidMiddleware } from "../../middlewares/ensureDataIsValid.middleware";
+import { ensureDataIsValidMiddleware, ensureDataIsValidUserForgotPasswordMiddleware } from "../../middlewares/ensureDataIsValid.middleware";
 import { updateSerializerProjects } from "../../serializers/projects/projects.serializer";
 import { ensureTechnologyMiddleware } from "../../middlewares/ensureTechnology.middleware";
 import {
   responseUserSerializer,
   updatedUserSerializer,
+  userForgotPasswordSerializer,
 } from "../../serializers/users/users.serializers";
 import { Users } from "../../entities/users.entity";
 import { ensureIdIsValidMiddleware } from "../../middlewares/ensureIdIsValid.middleware";
+import { userForgotPasswordController } from "../../controllers/users/userForgotPassword.controller";
 
 export const userRoutes = Router();
 
@@ -55,4 +57,10 @@ userRoutes.delete(
   ensureIsAdminMiddleware,
   ensureIdIsValidMiddleware(Users),
   deleteUserController
+);
+
+userRoutes.post(
+  "/forgotpassword",
+  ensureDataIsValidUserForgotPasswordMiddleware(userForgotPasswordSerializer),
+  userForgotPasswordController
 );
